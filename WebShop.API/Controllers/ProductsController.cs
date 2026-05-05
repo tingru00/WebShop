845b81d13624b1f016258f7e875ec35c34106410
tingru00 <grundbergtindra@gmail.com>
2026-05-05 10:49:50 +0200

    Added CreateProduct endpoint with CQRS and EF Core. Tested in Swagger. Also created an SQL database.

---
+++ WebShop.API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebShop.Application.Features.Products.Commands;
+
+namespace WebShop.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ProductsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateProductCommand command)
+    {
+        var id = await _mediator.Send(command);
+
+        return Ok(id);
+    }
+}
--- WebShop.API/Program.cs
+++ WebShop.API/Program.cs
@@ -1,26 +1,34 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using WebShop.Application.Features.Products.Commands;
+using WebShop.Domain.Entities;
+using WebShop.Domain.Interfaces;
 using WebShop.Infrastructure.Data;
 using WebShop.Infrastructure.Repositories;
-using WebShop.Domain.Interfaces;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Database connection string och DbContext registreras
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=WebShopDb;Trusted_Connection=True;"));
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+// MediatR och handlers registreras automatiskt via assembly scanning
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(CreateProductCommand).Assembly));
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
