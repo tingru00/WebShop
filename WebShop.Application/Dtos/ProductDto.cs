51af1459e77fd6d69f23a722d90fd72852e661d7
tingru00 <grundbergtindra@gmail.com>
2026-05-06 10:28:20 +0200

    Added DTO and AutoMapper for Products.

---
+++ WebShop.Application/Dtos/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebShop.Application.DTOs;
+
+public class ProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+}
---
+++ WebShop.Application/Mapping/ProductProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using WebShop.Domain.Entities;
+using WebShop.Application.DTOs;
+
+namespace WebShop.Application.Mapping;
+
+public class ProductProfile : Profile
+{
+    public ProductProfile()
+    {
+        CreateMap<Product, ProductDto>().ReverseMap();
+    }
+}
